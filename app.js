80fab96b361716986a3154f7337b709c0484a6e3
Evetta Banks <evettawashington@yahoo.com>
2024-05-22 21:35:55 -0500

    uppercase string javasript code wars

---
+++ app.js
@@ -0,0 +1,18 @@
+//Task
+//Create a method to see whether the string is ALL CAPS.
+
+/*
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+*/
+
+//Answer//
+
+String.prototype.isUpperCase = function() {
+    return this.toUpperCase() === this.toString();
+  }
